0483873853b184938e8591b55d29711e8df51bb4
Tanner Terry <terryt@civicinc.com>
2018-10-22 11:05:55 -0500

    NASH-2078-BugFix: GenerateRandomCode returns 4 characters instead of 5

--- Civic.Core.Imaging/CaptchaImgGenerator.cs
+++ Civic.Core.Imaging/CaptchaImgGenerator.cs
@@ -8,47 +8,23 @@ namespace Civic.Core.Imaging
 {
     public static class CaptchaImgGenerator
     {
+        //Note: This will never use L, I, 1, 0, or O because the resulting image is confusing for end users
+        private const string chars = "ABCDEFGHJKMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz23456789";
 
         public static Dictionary<string,RandomImage> GetRandomImage()
         {
             var result = new Dictionary<string, RandomImage>();
-            var capText = GenerateRandomCode();
+            var capText = GenerateRandomCode(5);
             RandomImage ci = new RandomImage(capText, 300, 75);
             result.Add(capText, ci);
             return result;
         }
         // Function to generate random string with Random class.
-        private static string GenerateRandomCode()
+        private static string GenerateRandomCode(int length)
         {
             Random r = new Random();
-            string s = "";
-            for (int j = 0; j < 5; j++)
-            {
-                int i = r.Next(3);
-                int ch;
-                switch (i)
-                {
-                    case 1:
-                        ch = r.Next(0, 9);
-                        s = s + ch.ToString();
-                        break;
-                    case 2:
-                        ch = r.Next(65, 90);
-                        s = s + Convert.ToChar(ch).ToString();
-                        break;
-                    case 3:
-                        ch = r.Next(97, 122);
-                        s = s + Convert.ToChar(ch).ToString();
-                        break;
-                    default:
-                        ch = r.Next(97, 122);
-                        s = s + Convert.ToChar(ch).ToString();
-                        break;
-                }
-                r.NextDouble();
-                r.Next(100, 1999);
-            }
-            return s;
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[r.Next(s.Length)]).ToArray());
         }
 
     }
