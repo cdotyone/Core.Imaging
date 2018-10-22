d000fa66cc452cf1f481635f997c78f990c793fb
ci <jenkins@CivicInc.local>
2018-10-22 09:48:37 -0500

    [ci skip]

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
 [assembly: AssemblyVersion("1.25.0.0")]
-[assembly: AssemblyFileVersion("1.25.0.3492")]
+[assembly: AssemblyFileVersion("1.25.1.3803")]
 [assembly: AssemblyCompany("Civic Engineering & IT")]
 [assembly: AssemblyCopyright("Copyright © 2018 Civic Engineering & IT")]
