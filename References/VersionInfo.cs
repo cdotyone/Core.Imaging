a9e4b4f246e130a7dd2ff59b0fff69f16f08c69c
Chris Doty <dotyc@civicinc.com>
2019-01-13 21:39:52 -0600

    feature: add sourcelink

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 [assembly: AssemblyVersion("1.25.0.0")]
 [assembly: AssemblyFileVersion("1.25.9.4220")]
 [assembly: AssemblyCompany("Civic Engineering & IT")]
-[assembly: AssemblyCopyright("Copyright © 2018 Civic Engineering & IT")]
+[assembly: AssemblyCopyright("Copyright © 2019 Civic Engineering & IT")]
